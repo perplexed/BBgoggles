3fb7159fc426bb6a683b813520f91f0929ad6292
Matt Stevens <01stevensm@gmail.com>
2012-01-20 15:48:48 +0000

    added copyleft notice (GPL)

--- src/bbgoggles/ResultsScreen.java
+++ src/bbgoggles/ResultsScreen.java
@@ -1,3 +1,18 @@
+/*This file is part of BBgoggles.
+
+BBgoggles is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BBgoggles is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BBgoggles.  If not, see <http://www.gnu.org/licenses/>.*/
+
 package bbgoggles;
 
 import net.rim.device.api.ui.Field;
